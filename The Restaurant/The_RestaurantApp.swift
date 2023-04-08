483e81e93d86dbfad7bcc8afcb053c62601d9ff6
Vladimer Redula <vdredula@gmail.com>
2023-04-08 19:11:01 +0900

    Initial Commit

---
+++ The Restaurant/The_RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_RestaurantApp.swift
+//  The Restaurant
+//
+//  Created by Vlad Redula on 2023/04/08.
+//
+
+import SwiftUI
+
+@main
+struct The_RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
